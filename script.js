bbf54706caacdb728d2a9a99aebb105fed17b09a
willisntannpc <wn.mugoto@gmail.com>
2024-08-02 14:04:40 +0300

    first commit: alot not done😂😂 been really lazy this last couple of weeks hope to finish up soon... i think or hope

---
+++ script.js
@@ -0,0 +1,10 @@
+
+$(document).ready(function(){
+     $('.slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+           arrows: true,
+           fade: true,
+           asNavFor: '.slider'
+        });
+});
